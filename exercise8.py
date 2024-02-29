35fed9d3890cd5fbec6dee01f7e9c2dca087f092
Abass Ibrahim <profbass4all@gmail.com>
2024-02-29 11:38:03 +0100

    I solved the exercise 9 challenge

--- exercise8.py
+++ exercise8.py
@@ -1,3 +1,7 @@
+# Make a two-player Rock-Paper-Scissors game. 
+# (Hint: Ask for player plays (using input), compare them, print out a message of 
+# congratulations to the winner, and ask if the players want to start a new game)
+
 win = False
 while win == False:
     player1= input('Enter your play1: ').lower()
---
+++ exercise9.py
@@ -0,0 +1,23 @@
+# Generate a random number between 1 and 9 
+# (including 1 and 9). Ask the user to guess the number, 
+# then tell them whether they guessed too low, too high, or exactly right.
+import random
+
+randNum = random.randint(1,9)
+isCorrect = False
+count = 0
+while isCorrect == False:
+    num = int(input('Enter your number: '))
+    count += 1
+    if(num < randNum):
+        print(f'{num} is less than the number. keep trying babe')
+        isCorrect = False
+    elif(num > randNum):
+        print(f'{num} is too high than the number you\'re searching for, keep trying babe')
+        isCorrect = False
+    elif(num == randNum):
+        print(f'finally! you got the number after you\'ve tried {count} times...congratulations')
+        isCorrect = True
+    else:
+        print(f'type a correct number')
+
